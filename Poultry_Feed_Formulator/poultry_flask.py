2586ad0bfaede47aad64879b5c9a339abf00b424
Colomu <101195888+Colomu@users.noreply.github.com>
2025-01-03 08:41:33 +0000

    Add files via upload

--- Poultry_Feed_Formulator/poultry_flask.py
+++ Poultry_Feed_Formulator/poultry_flask.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, jsonify
-from poultry_formulation import get_formulation, nutrient_requirements, ingredient_db
+from .poultry_formulation import get_formulation, nutrient_requirements, ingredient_db
 
 app = Flask(__name__)
 
